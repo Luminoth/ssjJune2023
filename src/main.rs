b7616b831783a17e34dfc51cd4591f73897d8919
Shane Lillie <ignignokterr@gmail.com>
2023-05-18 15:32:19 -0700

    Split out into modules

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
+mod components;
+mod plugins;
+mod resources;
+mod systems;
 
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
+use plugins::*;
 
 fn main() {
     App::new()
