7838ffb7788f52b987efb02c09eff5695f143384
Shane Lillie <ignignokterr@gmail.com>
2023-06-12 10:18:14 -0700

    Backend will use axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,35 @@
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+fn init_logging() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)?;
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    init_logging()?;
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
