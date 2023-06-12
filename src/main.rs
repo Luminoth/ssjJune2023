f45a9046c60496413bea986348c907994c097b01
Shane Lillie <ignignokterr@gmail.com>
2023-06-11 18:17:15 -0700

    Split game into client / server

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod systems;
 
 use bevy::prelude::*;
 
-use plugins::splash::*;
 use states::*;
 
 /*fn setup(mut commands: Commands) {
@@ -16,19 +15,38 @@ use states::*;
 }*/
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "ssjJune2023".into(),
-                ..default()
-            }),
+    #[cfg(all(feature = "client", feature = "server"))]
+    compile_error!("feature \"client\" and feature \"server\" cannot be enabled at the same time");
+
+    #[cfg(feature = "client")]
+    println!("starting client");
+
+    #[cfg(feature = "server")]
+    println!("starting server");
+
+    let mut app = App::new();
+    app.add_state::<GameState>();
+
+    #[cfg(feature = "client")]
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "ssjJune2023".into(),
             ..default()
-        }))
-        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+        }),
+        ..default()
+    }))
+    //.insert_resource(ClearColor(Color::BLACK))
+    .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+    .add_plugin(plugins::splash::SplashPlugin);
+
+    #[cfg(feature = "server")]
+    app.insert_resource(bevy::app::ScheduleRunnerSettings::run_loop(
+        bevy::utils::Duration::from_secs_f64(1.0 / 60.0),
+    ))
+    .add_plugins(MinimalPlugins);
+
+    app
         //.add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
-        .add_state::<GameState>()
-        .add_plugin(SplashPlugin)
-        /*.insert_resource(ClearColor(Color::BLACK))
-        .add_startup_system(setup)*/
+        //.add_startup_system(setup)
         .run();
 }
