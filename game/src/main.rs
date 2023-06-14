032ca92f5cd369470abc534d2aa9b4e77852595a
Shane Lillie <ignignokterr@gmail.com>
2023-06-14 12:32:29 -0700

    Add egui and cleanup app init

--- game/src/main.rs
+++ game/src/main.rs
@@ -9,49 +9,91 @@ mod resources;
 mod states;
 mod systems;
 
-use bevy::prelude::*;
+use bevy::{input::common_conditions::input_toggle_active, prelude::*};
+use bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use resources::Random;
 use states::*;
+
+const LOG_LEVEL: bevy::log::Level = bevy::log::Level::INFO;
 
 fn main() {
     #[cfg(all(feature = "client", feature = "server"))]
     compile_error!("feature \"client\" and feature \"server\" cannot be enabled at the same time");
 
     let mut app = App::new();
-    app.add_state::<GameState>()
-        //.add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default())
-        .insert_resource(Random::default());
+
+    // main game state has to come before plugins
+    app.add_state::<GameState>();
+
+    // core bevy plugins
+    //app.add_plugin(bevy::diagnostic::LogDiagnosticsPlugin::default());
 
     #[cfg(feature = "client")]
     {
         println!("starting client");
-        app.add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "ssjJune2023".into(),
-                ..default()
-            }),
-            ..default()
-        }))
-        //.insert_resource(ClearColor(Color::BLACK))
-        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
-        .add_plugin(plugins::client::splash::SplashPlugin)
-        .add_plugin(plugins::client::main_menu::MainMenuPlugin);
+
+        // core bevy plugins
+        app.add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "ssjJune2023".to_owned(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    watch_for_changes: true,
+                    ..Default::default()
+                })
+                .set(bevy::log::LogPlugin {
+                    level: LOG_LEVEL,
+                    ..Default::default()
+                }),
+        )
+        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin);
+
+        // client plugins
+        app.add_plugin(plugins::client::splash::SplashPlugin)
+            .add_plugin(plugins::client::main_menu::MainMenuPlugin);
     }
 
     #[cfg(feature = "server")]
     {
         println!("starting server");
+
+        // core bevy plugins
         app.insert_resource(bevy::app::ScheduleRunnerSettings::run_loop(
             bevy::utils::Duration::from_secs_f64(1.0 / 60.0),
         ))
         .add_plugins(MinimalPlugins)
-        .add_plugin(bevy::log::LogPlugin::default())
-        .add_plugin(bevy_tokio_tasks::TokioTasksPlugin::default())
-        .add_plugin(plugins::server::init::InitServerPlugin)
-        .add_plugin(plugins::server::looking_for_work::LookingForWorkPlugin)
-        .add_plugin(plugins::server::working::WorkingPlugin);
+        // TODO: do we need the asset plugin?
+        .add_plugin(bevy::log::LogPlugin {
+            level: LOG_LEVEL,
+            ..Default::default()
+        });
+
+        // tokio runtime
+        app.add_plugin(bevy_tokio_tasks::TokioTasksPlugin::default());
+
+        // server plugins
+        app.add_plugin(plugins::server::init::InitServerPlugin)
+            .add_plugin(plugins::server::looking_for_work::LookingForWorkPlugin)
+            .add_plugin(plugins::server::working::WorkingPlugin);
     }
+
+    // egui
+    app.add_plugin(EguiPlugin);
+
+    // inspector
+    app.add_plugin(
+        WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Grave)),
+    );
+
+    // shared resources
+    app.insert_resource(Random::default());
 
     app.run();
 }
