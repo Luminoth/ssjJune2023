656332aaf30db23a930c32e93cc2f491fe850978
Shane Lillie <ignignokterr@gmail.com>
2023-08-27 11:54:12 -0700

    cleanup?

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -2,14 +2,15 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use tracing::error;
 
 #[derive(Debug)]
 pub struct AppError(pub anyhow::Error);
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        // TODO: this doesn't seem like the best place to print this?
-        tracing::error!("internal error: {}", self.0);
+        // TODO: this doesn't seem like the best place to log this?
+        error!("internal error: {}", self.0);
         (StatusCode::INTERNAL_SERVER_ERROR, "internal error").into_response()
     }
 }
